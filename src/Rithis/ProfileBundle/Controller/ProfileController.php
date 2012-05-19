8a2dd199b0f65d4346344503ffabe4c79b18c5fc
Alexander Kondratenko <a.kondratenko@pegast.ru>
2012-05-19 18:58:27 +0400

    Some fixes in ProfileController

--- src/Rithis/ProfileBundle/Controller/ProfileController.php
+++ src/Rithis/ProfileBundle/Controller/ProfileController.php
@@ -11,14 +11,57 @@ use FOS\RestBundle\View\View;
 use FOS\RestBundle\View\RouteRedirectView;
 use FOS\Rest\Util\Codes;
 
+use Rithis\ProfileBundle\Form\ProfileForm;
+
 class ProfileController extends Controller
 {
-    public function newRequestsAction(Request $request)
+    public function profileAction(Request $request)
     {
+//        $profile = array();
+        $form = $this->createForm(new ProfileForm($this->container));
         $view = View::create();
-        $view->setData($this->getNewRequestsData($request));
-        $view->setTemplate(new TemplateReference('RithisProfileBundle', 'Requests', 'newRequests'));
+        $view->setData(array('form' => $form));
+        $view->setTemplate(new TemplateReference('RithisProfileBundle', 'Profile', 'profile'));
 
         return $view;
+    }
+
+    public function postProfileAction(Request $request) {
+        $form = $this->createForm(new ProfileForm($this->container));
+        $form->bindRequest($request);
+
+        if ($form->isValid()) {
+//            $query = $form->getData();
+//
+//            $result = array_values(iterator_to_array($this->get('mongodb')->hotels->find($this->convertToMongoQuery($query))));
+//
+//            $searchRequest = array(
+//                'query' => $query,
+//                'result' => $result
+//            );
+//
+//            $this->get('mongodb')->search_requests->insert($searchRequest);
+
+            return RouteRedirectView::create('profile_get_request', array('ok' => true));
+        }
+
+        $view = View::create();
+        $view->setData($this->getNewRequestsData($request, $form));
+        $view->setStatusCode(Codes::HTTP_BAD_REQUEST);
+        $view->setTemplate(new TemplateReference('RithisAlexisBundle', 'Profile', 'profile'));
+
+        return $view;
+    }
+
+    private function getNewRequestsData(Request $request, FormInterface $form = null)
+    {
+        if ($request->getRequestFormat() == 'html') {
+            $form = $form ?: $this->createForm(new ProfileForm($this->container));
+            return array('form' => $form);
+        }
+//        else {
+//            $countries = $this->get('alexis.form.choicelist.country')->getValues();
+//            return array('countries' => $countries);
+//        }
     }
 }
